3dfa5512836190c698697d3ea271e8a73d423c07
Jatin Singh <97218897+jatincodes595@users.noreply.github.com>
2022-01-30 21:54:01 +0530

    updated

--- Walmart/12.Find Array Given Subset Sums.cpp
+++ Walmart/12.Find Array Given Subset Sums.cpp
@@ -1,36 +1,53 @@
-class Solution{
-
-  public:
-  int sumTotal;
-  vector<vector<int>> dp;
-    int findMinRec(int arr[], int i, int sumCal)
+class Solution {
+public:
+    //// this algo is iterative version of divide and conquer
+    // try to dry run this code on paper so that you can get the idea wisely
+    vector<int> recoverArray(int n, vector<int>& sums) 
     {
-        // If we have reached last element.  Sum of one
-        // subset is sumCalculated, sum of other subset is
-        // sumTotal-sumCalculated.  Return absolute difference
-        // of two sums.
-        if (i == 0)
-            return abs((sumTotal - sumCal)- sumCal);
-     
-        if(dp[i][sumCal]!=-1)
+        // first sort the array
+        sort(sums.begin(), sums.end()); 
+        
+        vector<int> ans; 
+        while (n--) 
         {
-            return dp[i][sumCal];
+            // either you take the diff of first two elemts or last 2 elemts 
+            // so we just need to decide wether -diff or +diff will be in the main array
+            int diff = sums[1] - sums[0]; 
+            unordered_map<int, int> freq; 
+            vector<int> ss0, ss1; 
+            bool on = false; // this will decide whether diff will be neg or pos
+            // we just need to make to array ss0 and ss1 so that one array can be converted to another array 
+            // by just adding or subtracting diff  to every value in one of the array i.e. ss0 or ss1
+            for (auto& x : sums) 
+            {
+                if (freq[x]==0) 
+                {
+                    ss0.push_back(x); 
+                    freq[x+diff]++; 
+                    // if x comes for the first time with 0 freq that means the diff is pos 
+                    // beacuse if prev + diff are able to make the 0 value then it is confirmed the array contains a -ve no
+                    if (x == 0) on = true; 
+                } 
+                else 
+                {
+                    ss1.push_back(x); 
+                    freq[x]--; 
+                }
+            }
+            if (on) 
+            {
+    // here +diff and ss0 represent if we subtract diff to every value of ss0 array then it will give ss1 array
+                ans.push_back(diff); 
+                sums = ss0; 
+                
+            } 
+            else 
+            {
+    // here -diff and ss1 represent if we add diff to every value of ss1 array then it will give ss0 array
+                ans.push_back(-diff); 
+                sums = ss1; 
+            }
         }
-        // For every item arr[i], we have two choices
-        // (1) We do not include it first set
-        // (2) We include it in first set
-        // We return minimum of two choices
-        dp[i][sumCal]=min(findMinRec(arr, i - 1, sumCal+ arr[i - 1]),findMinRec(arr, i - 1, sumCal));
-        return dp[i][sumCal];
+        return ans; 
     }
-	int minDifference(int arr[], int n)  
-	{ 
-	    // Compute total sum of elements
-        sumTotal = 0;
-        for (int i = 0; i < n; i++)
-            sumTotal += arr[i];
- 
-        dp.resize(n+1,vector<int>(sumTotal+1,-1));
-        return findMinRec(arr, n, 0);
-	} 
 };
